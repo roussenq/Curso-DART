d09e99bdbd9e43603e251ad70e13b1f0afac3f31
David <davidroussenq@gmail.com>
2025-03-05 17:11:17 -0300

    chamada de função

--- BASICO/funcoes.dart
+++ BASICO/funcoes.dart
@@ -1,8 +1,15 @@
-void man() {
+void main() {
   boasVindas();
+  double calculo = simulador(10.5, 22.76);
+  print(calculo);
 }
 
 void boasVindas() {
   String nome = 'David';
   print('Seja bem vindo(a), ' + nome);
+}
+
+double simulador(double a, b) {
+  double resultado = a + b;
+  return resultado;
 }
